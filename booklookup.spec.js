54383a3724bb5581aed2ac3b20d1028a5e4ef4b8
nattapong <58160388@go.buu.ac.th>
2017-10-25 12:19:40 +0700

    Not tested is codeing

--- booklookup.spec.js
+++ booklookup.spec.js
@@ -4,3 +4,17 @@ test('tsst', () => {
     expect(test).toBe("test")
 
 }) 
+
+
+test ('testbook-lookup',() => {
+
+    const amazonservice = jest.fn('aaaaasssssddd').mockReturnValue({ name: 'javascript-book', cover: 'image.com', ispn:'aaaaasssssddd'})
+
+    let app = new booklookup(amazonservice)
+    let ispn = "aaaaasssssddd"
+    let result = app.search(ispn)
+
+
+
+
+})
